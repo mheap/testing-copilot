8390d8f9f33aee57d19d8c9abcf31638f1cea5c2
Michael Heap <m@michaelheap.com>
2025-07-28 13:15:10 +0100

    Build working

--- packages/app/dist-electron/preload.cjs
+++
@@ -1,8 +0,0 @@
-"use strict";
-const { contextBridge, ipcRenderer } = require("electron");
-contextBridge.exposeInMainWorld("electronAPI", {
-  registerUser: (userData) => ipcRenderer.invoke("register-user", userData),
-  loginUser: () => ipcRenderer.invoke("login-user"),
-  getCurrentUser: () => ipcRenderer.invoke("get-current-user"),
-  logout: () => ipcRenderer.invoke("logout")
-});
